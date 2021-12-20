aaaa0caf7ae84cd03e6a4f0bf6d712c356bd58a6
g4m3r-x <SlavaSaz@inbox.ru>
2021-12-20 13:46:03 +0700

    add authorization vk

--- selenium_python/ChromeDriver/main.py
+++ selenium_python/ChromeDriver/main.py
@@ -1,14 +1,26 @@
 from selenium import webdriver
 import time
 
-driver = webdriver.Chrome(executable_path="D:\\github_practice\\practice\\selenium_python\\ChromeDriver\\chromedriver.exe")
+driver = webdriver.Chrome(executable_path=r"D:\github_practice\practice\selenium_python\ChromeDriver\chromedriver.exe")
 
 url = "https://www.vk.com/"
 
 try:
     driver.get(url=url)
     time.sleep(5)
-    driver.get
+    email_input = driver.find_element_by_id("index_email")
+    email_input.clear()
+    email_input.send_keys("+79670333350")
+    time.sleep(3)
+    # -------------------
+    pass_input = driver.find_element_by_id("index_pass")
+    pass_input.clear()
+    pass_input.send_keys("GaryGuls")
+    time.sleep(3)
+    # -------------------
+    button = driver.find_element_by_id("index_login_button").click()
+    time.sleep(10)
+
 except Exception as ex:
     print(ex)
 finally:
